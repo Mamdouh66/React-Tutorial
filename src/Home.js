82495701b73ba3734daffaa91dc2d733c368e5d8
Mamdouh66 <2210002016@iau.edu.sa>
2023-08-11 18:52:49 +0300

    finished lesson 1-8

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Home = () => {
+  const [name, setName] = useState("mario");
+
+  const handleClick = () => {
+    setName("luigi");
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>click me</button>
+    </div>
+  );
+};
+
+export default Home;

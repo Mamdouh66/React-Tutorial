91e335101b76651114ca94f3526df470f960408a
Mamdouh66 <2210002016@iau.edu.sa>
2023-08-11 19:44:01 +0300

    finishd lesson 8-13

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+const Bloglist = (props) => {
+  const blogs = props.blogs;
+  const title = props.title;
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <button onClick={() => props.handleDelete(blog.id)}>
+              delete blog
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Bloglist;
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,29 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-  const handleClick = () => {
-    setName("luigi");
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
   };
-
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>click me</button>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+      />
     </div>
   );
 };
